c11159de4628ef2cd37d1537b1cc3b09c3df8fb7
Julien Mastromonaco <mastromonaco.julien@gmail.com>
2019-05-15 23:55:22 +0200

    fix: Update utils module, add prepareOptions method + tests

--- src/utils.js
+++ src/utils.js
@@ -7,6 +7,20 @@ function isPlainObject (arg) {
   return typeof arg === 'object' && !Array.isArray(arg) && arg !== null
 }
 
+/**
+ * Check and prepare options object.
+ * @param {object} options - The options object.
+ * @param {object} reference - The reference options object.
+ */
+function prepareOptions (options, reference) {
+  if (!isPlainObject(options)) {
+    throw new TypeError('The "options" argument must be a plain object.')
+  }
+
+  return { ...reference, ...options }
+}
+
 module.exports = {
-  isPlainObject
+  isPlainObject,
+  prepareOptions
 }
--- test/specs/utils.spec.js
+++ test/specs/utils.spec.js
@@ -12,3 +12,21 @@ describe('isPlainObject()', () => {
     expect(utils.isPlainObject(null)).toBe(false)
   })
 })
+
+describe('prepareOptions()', () => {
+  test('done', () => {
+    expect(utils.prepareOptions({ a: null }))
+      .toEqual({ a: null })
+
+    expect(utils.prepareOptions({ a: 'ok', c: 'ok' }, { a: null, b: 'ok' }))
+      .toEqual({ a: 'ok', b: 'ok', c: 'ok' })
+  })
+
+  test('fail', () => {
+    expect(() => utils.prepareOptions())
+      .toThrowError('The "options" argument must be a plain object.')
+
+    expect(() => utils.prepareOptions([]))
+      .toThrowError('The "options" argument must be a plain object.')
+  })
+})

2d56326eb1642761efdd5d553dd412e8d7f9572f
Julien Mastromonaco <mastromonaco.julien@gmail.com>
2019-05-16 19:29:13 +0200

    test: Add test for ChromaticWrapper

--- test/specs/index.spec.js
+++ test/specs/index.spec.js
@@ -1,7 +1,35 @@
-describe('Test Temp', () => {
-  const index = require('../../src/index.js')
+const Chromatic = require('../../src/index.js')
+const Consolizer = require('../../src/consolizer.js')
+const Stylizer = require('../../src/stylizer.js')
 
-  test('temp', () => {
-    expect(index).not.toBe(false)
+describe('Chromatic', () => {
+  test('constructor', () => {
+    expect(() => new Chromatic()).not.toThrowError()
+  })
+
+  test('getConsolizer()', () => {
+    const chromatic = new Chromatic()
+    expect(chromatic.getConsolizer()).toBeInstanceOf(Consolizer)
+  })
+
+  test('getStylizer()', () => {
+    const chromatic = new Chromatic()
+    expect(chromatic.getStylizer()).toBeInstanceOf(Stylizer)
+  })
+
+  test('common method should not throw', () => {
+    const chromatic = new Chromatic()
+    const stylizer = chromatic.getStylizer()
+    const hidder = (value) => stylizer.stylize(
+      stylizer.styles.modifiers.hidden,
+      value
+    )
+
+    expect(() => {
+      chromatic.bright(hidder('Hello'))
+      chromatic.underline(hidder('Hello'))
+      chromatic.yellow(hidder('Hello'))
+      chromatic.black(hidder('Hello'))
+    }).not.toThrowError()
   })
 })
